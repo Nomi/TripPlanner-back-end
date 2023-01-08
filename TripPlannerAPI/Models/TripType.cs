6f752a329a654cd8a0586d3234ec3c8458d4408e
Nader Tavana <nadertavana888@gmail.com>
2023-01-08 21:30:11 +0100

    Add TripType Model

---
+++ TripPlannerAPI/Models/TripType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TripPlannerAPI.Models
+{
+    public class TripType
+    {
+        public int Id { get; set; }
+        [Required]
+        [NotNull]
+        public string? TypeName { get; set; }
+    }
+}
