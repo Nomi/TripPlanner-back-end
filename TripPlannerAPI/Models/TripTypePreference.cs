c1cddbc91ebb827071a7a94d338a50403d983eca
Nader Tavana <nadertavana888@gmail.com>
2023-01-08 21:30:45 +0100

    Add TripTypePreferences Model

---
+++ TripPlannerAPI/Models/TripTypePreference.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TripPlannerAPI.Models
+{
+    public class TripTypePreference
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TripTypeId { get; set; }
+        public TripType TripType { get; set; }
+        public int PreferenceTypeId { get; set; }
+        public PreferenceType PreferenceType { get; set; }
+        public User User{ get; set; }
+        public int Points { get; set; }
+    }
+}
